52a40ed68fb974e4dff9d20c5e81b5ea564fd18c
mocacinno <mocacinno@gmx.com>
2023-02-20 08:25:36 +0000

    added basic build commands
    Not really structured the way i'd like them, but it's a quick start

---
+++ Build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#go tool dist list
+rm builds/*
+GOOS=linux go build -ldflags="-s -w" -o builds/logfileparser.tmp logfileparser.go
+GOOS=linux go build -ldflags="-s -w" -o builds/stats.tmp stats.go
+upx -f --brute -o builds/logfileparser builds/logfileparser.tmp
+upx -f --brute -o builds/stats builds/stats.tmp
+cp config.template.ini builds/config.ini
+tar cvzf builds/clftosqlite-v$1-linux-amd64.tar.gz builds/logfileparser builds/stats builds/config.ini
+GOOS=windows GOARCH=amd64 go build -o builds/logfileparser.exe logfileparser.go
+GOOS=windows GOARCH=amd64 go build -o builds/stats.exe stats.go
+7zr a builds/clftosqlite-v$1-windows-amd64.zip builds/*.exe
