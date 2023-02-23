5b02cb697403cefd13a9ee9ef3c28ce06c7544b4
mocacinno <mocacinno@gmx.com>
2023-02-23 08:30:54 +0000

    local build procedure adapted

--- Build.sh
+++ Build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #go tool dist list
+sed "s/vmyversion/v$1/" nfpm.yaml.template > nfpm.yaml
 rm builds/*
 GOOS=linux go build -ldflags="-s -w" -o builds/logfileparser.tmp logfileparser.go
 GOOS=linux go build -ldflags="-s -w" -o builds/stats.tmp stats.go
@@ -10,5 +11,12 @@ tar cvzf builds/clftosqlite-v$1-linux-amd64.tar.gz builds/logfileparser builds/s
 GOOS=windows GOARCH=amd64 go build -o builds/logfileparser.exe logfileparser.go
 GOOS=windows GOARCH=amd64 go build -o builds/stats.exe stats.go
 7zr a builds/clftosqlite-v$1-windows-amd64.zip builds/*.exe builds/config.ini
+nfpm pkg --packager deb --target /root/CLFtoSQLite/builds/
+nfpm pkg --packager rpm --target /root/CLFtoSQLite/builds/
+gpg --detach-sign builds/*.deb
+gpg --detach-sign builds/*.rpm
 gpg --detach-sign builds/clftosqlite-v$1-linux-amd64.tar.gz
 gpg --detach-sign builds/clftosqlite-v$1-windows-amd64.zip
+rm builds/stats*
+rm builds/logfileparser*
+rm builds/*.ini
